c0a4bc9b3cbde71e30a4502a3b0e09cae3dab348
kanake10 <10kanake@gmail.com>
2022-11-02 22:33:08 +0300

    genre,artist and album daos

---
+++ app/src/main/java/com/swayy/musicpark/data/local/dao/GenreDao.kt
@@ -0,0 +1,20 @@
+package com.swayy.musicpark.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.swayy.musicpark.data.local.entity.GenreEntity
+
+
+@Dao
+interface GenreDao {
+    @Query("SELECT * FROM genre_table")
+    suspend fun getGenre():List<GenreEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertGenres(list: List<GenreEntity>)
+
+    @Query("DELETE FROM genre_table")
+    suspend fun deleteGenres()
+}
