c06c3eea9ccf4cada4b457eb8dff917e2933a3a6
kanake10 <10kanake@gmail.com>
2022-11-02 16:21:17 +0300

    init timber

--- app/src/main/java/com/swayy/musicpark/presentation/NapsterApp.kt
+++ app/src/main/java/com/swayy/musicpark/presentation/NapsterApp.kt
@@ -2,6 +2,16 @@ package com.swayy.musicpark.presentation
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class NapsterApp: Application()
+class NapsterApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        setupTimber()
+    }
+}
+
+private fun setupTimber() {
+    Timber.plant(Timber.DebugTree())
+}
